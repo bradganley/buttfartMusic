215db4a09b9f9a3d0ebb5ea7cdfb206f163d1b63
Brad Ganley <bradart.ink@gmail.com>
2021-01-25 01:48:21 -0600

    added genre tooltip on hover

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button'
-import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const assembleArtist = (artist, ttLink, spotifyLink, ytLink) =>{
-  return { artist, ttLink, spotifyLink, ytLink }
+const assembleArtist = (artist, ttLink, spotifyLink, ytLink, genre) =>{
+  return { artist, ttLink, spotifyLink, ytLink, genre }
 }
 
 const rows = [
-  assembleArtist('Broken at Best', 'https://www.tiktok.com/@brokenatbest', 'https://open.spotify.com/artist/4CJazbFqEso8BiGP10gz9L?si=V7_KtkpZQyWmFjx5gJb5xg'),
-  assembleArtist('Skatune Network ', 'https://www.tiktok.com/@skatunenetwork', 'https://open.spotify.com/artist/7Gas5IrI1Y8PSrJPyRkwtC?si=k7Xey1B6RV2OQPCyYYjO_g','https://www.youtube.com/channel/UCji2l5wcs6GoYJY1GgG_slQ')
+  assembleArtist('Broken at Best', 'https://www.tiktok.com/@brokenatbest', 'https://open.spotify.com/artist/4CJazbFqEso8BiGP10gz9L?si=V7_KtkpZQyWmFjx5gJb5xg',null, 'Acoustic'),
+  assembleArtist('Skatune Network ', 'https://www.tiktok.com/@skatunenetwork', 'https://open.spotify.com/artist/7Gas5IrI1Y8PSrJPyRkwtC?si=k7Xey1B6RV2OQPCyYYjO_g','https://www.youtube.com/channel/UCji2l5wcs6GoYJY1GgG_slQ', 'Ska'),
+  assembleArtist('Adjust the Sails', 'https://vm.tiktok.com/ZMJt7gcnW/', 'https://hypeddit.com/link/oxn8il#spotify','https://youtu.be/5mMbAQ9jew0', 'Midwest Emo'),
+  assembleArtist('The Upfux', null, null, null, 'Aggressively Ska')
 ]
 
 function App() {
@@ -37,14 +40,16 @@ function App() {
         <TableBody>
           {rows.map(row => 
             ( <TableRow key={row.artist}>
-            <TableCell component="th" scope="row">
-              {row.artist}
+            <TableCell align="center">
+              <Tooltip placement="bottom-end" title={row.genre} interactive arrow>
+              <div>{row.artist}</div>
+              </Tooltip>
             </TableCell>
             <TableCell align="center">
             <Button variant="contained" onClick={()=> window.open(row.ttLink, 'tiktok')}>TikTok</Button>
             </TableCell>
             <TableCell align="center"><Button variant="contained" onClick={()=> window.open(row.spotifyLink, 'Spotify')}>Spotify</Button></TableCell>
-            <TableCell align="center"><Button variant="contained" onClick={()=> window.open(row.yt, 'Youtube')}>Youtube</Button></TableCell>
+            <TableCell align="center"><Button disabled={!row.ytLink}variant="contained" onClick={()=> window.open(row.ytLink, 'Youtube')}>Youtube</Button></TableCell>
           </TableRow> )
           )}
         </TableBody>
