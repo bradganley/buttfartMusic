c7f17aa81d1cc752a3dea35ca3331b5317fa70ce
Brad Ganley <bradart.ink@gmail.com>
2021-01-25 01:54:12 -0600

    qol

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ const assembleArtist = (artist, ttLink, spotifyLink, ytLink, genre) =>{
 }
 
 const rows = [
-  assembleArtist('Broken at Best', 'https://www.tiktok.com/@brokenatbest', 'https://open.spotify.com/artist/4CJazbFqEso8BiGP10gz9L?si=V7_KtkpZQyWmFjx5gJb5xg',null, 'Acoustic'),
+  assembleArtist('Broken at Best', 'https://www.tiktok.com/@brokenatbest', 'https://open.spotify.com/artist/4CJazbFqEso8BiGP10gz9L?si=V7_KtkpZQyWmFjx5gJb5xg','https://www.youtube.com/channel/UCnUNOc1gKJ0tWfD9MaY_Fvw', 'Acoustic'),
   assembleArtist('Skatune Network ', 'https://www.tiktok.com/@skatunenetwork', 'https://open.spotify.com/artist/7Gas5IrI1Y8PSrJPyRkwtC?si=k7Xey1B6RV2OQPCyYYjO_g','https://www.youtube.com/channel/UCji2l5wcs6GoYJY1GgG_slQ', 'Ska'),
   assembleArtist('Adjust the Sails', 'https://vm.tiktok.com/ZMJt7gcnW/', 'https://hypeddit.com/link/oxn8il#spotify','https://youtu.be/5mMbAQ9jew0', 'Midwest Emo'),
-  assembleArtist('The Upfux', null, null, null, 'Aggressively Ska')
+  assembleArtist('The Upfux', 'https://www.tiktok.com/@juul.thief?lang=en','https://open.spotify.com/artist/5xdshsLn3F7WL9FhC3aJoU?si=C-gXpeSrRP2Sw20pGjE-3w', null, 'Aggressively Ska')
 ]
 
 function App() {
@@ -46,10 +46,10 @@ function App() {
               </Tooltip>
             </TableCell>
             <TableCell align="center">
-            <Button variant="contained" onClick={()=> window.open(row.ttLink, 'tiktok')}>TikTok</Button>
+            <Button disabled={!row.ttLink} variant="contained" onClick={()=> window.open(row.ttLink, 'tiktok')}>TikTok</Button>
             </TableCell>
-            <TableCell align="center"><Button variant="contained" onClick={()=> window.open(row.spotifyLink, 'Spotify')}>Spotify</Button></TableCell>
-            <TableCell align="center"><Button disabled={!row.ytLink}variant="contained" onClick={()=> window.open(row.ytLink, 'Youtube')}>Youtube</Button></TableCell>
+            <TableCell align="center"><Button disabled={!row.spotifyLink} variant="contained" onClick={()=> window.open(row.spotifyLink, 'Spotify')}>Spotify</Button></TableCell>
+            <TableCell align="center"><Button disabled={!row.ytLink} variant="contained" onClick={()=> window.open(row.ytLink, 'Youtube')}>Youtube</Button></TableCell>
           </TableRow> )
           )}
         </TableBody>
