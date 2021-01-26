c9d87342d5e877305c59db8e3970dc72edd2ba68
Brad Ganley <bradart.ink@gmail.com>
2021-01-26 02:37:59 -0600

    lol actual cleanup this time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-//import logo from './logo.svg';
 import './App.css';
-
 import React from 'react'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -13,16 +11,6 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Tooltip from '@material-ui/core/Tooltip';
 
-//const assembleArtist = (artist, ttLink, spotifyLink, ytLink, genre) =>{
-//return { artist, ttLink, spotifyLink, ytLink, genre }
-//}
-
-//const rows = [
-//  assembleArtist('Broken at Best', 'https://www.tiktok.com/@brokenatbest', 'https://open.spotify.com/artist/4CJazbFqEso8BiGP10gz9L?si=V7_KtkpZQyWmFjx5gJb5xg','https://www.youtube.com/channel/UCnUNOc1gKJ0tWfD9MaY_Fvw', 'Acoustic'),
-//  assembleArtist('Skatune Network ', 'https://www.tiktok.com/@skatunenetwork', 'https://open.spotify.com/artist/7Gas5IrI1Y8PSrJPyRkwtC?si=k7Xey1B6RV2OQPCyYYjO_g','https://www.youtube.com/channel/UCji2l5wcs6GoYJY1GgG_slQ', 'Ska'),
-//  assembleArtist('Adjust the Sails', 'https://vm.tiktok.com/ZMJt7gcnW/', 'https://hypeddit.com/link/oxn8il#spotify','https://youtu.be/5mMbAQ9jew0', 'Midwest Emo'),
-//  assembleArtist('The Upfux', 'https://www.tiktok.com/@juul.thief?lang=en','https://open.spotify.com/artist/5xdshsLn3F7WL9FhC3aJoU?si=C-gXpeSrRP2Sw20pGjE-3w', null, 'Aggressively Ska')
-//]
 class App extends React.Component {
   constructor(props){
     super(props);
@@ -53,20 +41,6 @@ class App extends React.Component {
           </TableRow>
         </TableHead>
         <TableBody>
-          {/*rows.map(row => 
-            ( <TableRow hover key={row.artist}>
-            <TableCell align="center">
-              <Tooltip enterTouchDelay='1' leaveTouchDelay='2000' placement="bottom-end" title={row.genre} interactive arrow>
-              <div>{row.artist}</div>
-              </Tooltip>
-            </TableCell>
-            <TableCell align="center">
-            <Button disabled={!row.ttLink} variant="outlined" color='default' onClick={()=> window.open(row.ttLink, 'tiktok')}>TikTok</Button>
-            </TableCell>
-            <TableCell align="center"><Button disabled={!row.spotifyLink} variant="outlined" color='primary' onClick={()=> window.open(row.spotifyLink, 'Spotify')}>Spotify</Button></TableCell>
-            <TableCell align="center"><Button disabled={!row.ytLink} variant="outlined" color='secondary' onClick={()=> window.open(row.ytLink, 'Youtube')}>Youtube</Button></TableCell>
-          </TableRow> )
-          )*/}
           {Object.entries(artists).map(([key, artist]) => {
               return ( 
                 <TableRow hover key={artist.name}  style={smoll}>
