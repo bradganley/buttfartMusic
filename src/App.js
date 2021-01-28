33e6a6410335836953641ecc83285ca109284137
Brad Ganley <bradart.ink@gmail.com>
2021-01-27 20:00:26 -0600

    prettier buttons

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
 import IconButton from '@material-ui/core/IconButton';
 import AutorenewRoundedIcon from '@material-ui/icons/AutorenewRounded';
 import Snackbar from '@material-ui/core/Snackbar';
+import { AwesomeButton } from 'react-awesome-button';
+import styles from 'react-awesome-button/src/styles/themes/theme-c137';
 
 
 class App extends React.Component {
@@ -32,11 +33,11 @@ class App extends React.Component {
     const smoll = { height: "10px", padding: "3px" };
   return (
     <center style={{'padding':'15px'}}>
-      <Paper style={{maxWidth:'900px','background-color': '#7d7d7d', 'padding':'15px'}} elevation={10}>
+      <Paper style={{maxWidth:'900px','backgroundColor': '#7d7d7d', 'padding':'15px'}} elevation={10}>
       <TableContainer style={{'maxWidth':'800px', 'padding':'15px'}}>
-      <Table style={{'background-color': '#f5f5f5','padding':'5px'}}>
+      <Table style={{'backgroundColor': '#f5f5f5','padding':'5px'}}>
         <TableHead>
-          <TableRow style={{'background-color': '#494949', 'outline-color':'white'}}>
+          <TableRow style={{'backgroundColor': '#494949', 'outlineColor':'white'}}>
             <TableCell style={{...smoll, 'color':'white'}} variant="head" align="center"><b>Artist</b></TableCell>
             <TableCell style={{...smoll, 'color':'white'}} variant="head" align="center"><b>TikTok Profile Link</b></TableCell>
             <TableCell style={{...smoll, 'color':'white'}} align="center"><b>Spotify Link</b></TableCell>
@@ -49,23 +50,23 @@ class App extends React.Component {
                 <TableRow hover key={artist.name}  style={smoll}>
                 <TableCell align="center" style={smoll}>
                   <Tooltip enterTouchDelay={1} leaveTouchDelay={2000} placement="bottom" title={artist.genre} interactive>
-                  <div style={{'font-size':'20'}}>{artist.name}</div>
+                  <div style={{'fontSize':'20'}}>{artist.name}</div>
                   </Tooltip>
                 </TableCell>
                 <TableCell style={smoll} align="center">
-                <Button style={{height:'20px', 'color':'white', 'background-color':'#69C9D0'}} size='small' disabled={!artist.tiktok} variant="contained" color='default' onClick={()=> window.open(artist.tiktok, 'tiktok')}>TikTok</Button>
+                <AwesomeButton size='small' disabled={!artist.tiktok} type='primary' onPress={()=> window.open(artist.tiktok, 'tiktok')}>TikTok</AwesomeButton>
                 </TableCell>
-                <TableCell style={smoll} align="center"><Button style={{height:'20px', 'background-color':'#1DB954'}} size='small' disabled={!artist.spotify} variant="contained" onClick={()=> window.open(artist.spotify, 'Spotify')}>Spotify</Button></TableCell>
-                <TableCell style={smoll} align="center"><Button style={{height:'20px', 'background-color':' #c4302b'}} size='small' disabled={!artist.yt} variant="contained" color='secondary' onClick={()=> window.open(artist.yt, 'Youtube')}>Youtube</Button></TableCell>
+                <TableCell style={smoll} align="center"><AwesomeButton size='small' disabled={!artist.spotify} type='secondary' onPress={()=> window.open(artist.tiktok, 'tiktok')}>Spotify</AwesomeButton></TableCell>
+                <TableCell style={smoll} align="center"><AwesomeButton size='small' disabled={!artist.yt} type='link' onPress={()=> window.open(artist.tiktok, 'tiktok')}>Youtube</AwesomeButton></TableCell>
               </TableRow> );
           })}
         </TableBody>
       </Table>
       </TableContainer>
-      <IconButton size='small' variant="outlined" color="primary" onClick={() => this.getDb() }><AutorenewRoundedIcon /></IconButton>
+      <IconButton size='small' variant="outlined" onClick={() => this.getDb() }><AutorenewRoundedIcon /></IconButton>
       </Paper> 
-      <Paper style={{maxWidth:'500px','background-color': '#052323', 'padding':'15px'}} elevation={10}> 
-      <iframe title='spoofydoodle' src="https://open.spotify.com/embed/playlist/1Gudo5j6pxf86CiP7E5Atq" width="400" height="600" frameborder="0" allowtransparency="false" allow="encrypted-media"></iframe>
+      <Paper style={{maxWidth:'500px','backgroundColor': '#052323', 'padding':'15px'}} elevation={10}> 
+      <iframe title='spoofydoodle' src="https://open.spotify.com/embed/playlist/1Gudo5j6pxf86CiP7E5Atq" width="400" height="600" frameBorder="0" allowtransparency="false" allow="encrypted-media"></iframe>
       </Paper>
       <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} message="List updated" open={this.state.showSnack} autoHideDuration={5000} onClose={() => this.setState({showSnack: false})} />
     </center>
